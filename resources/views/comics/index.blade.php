e7eff92573250783861e32ade72e525c9ce7379d
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-08 17:16:49 +0100

    remove @dump

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,18 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Elenco Comics</title>
+    <title>Homepage Comics</title>
 </head>
 
 <body>
 
-    @dump($comics);
-    @foreach ($comics as $comic)
-        <div>
-            <a href="{{ route('comics.show', $comic->id) }}"> {{ $comic->title }} </a>
-        </div>
-    @endforeach
-
+    {{-- @dump($comics); --}}
+    <div class="contenitore_homepage">
+        @foreach ($comics as $comic)
+            <div>
+                <a href="{{ route('comics.show', $comic->id) }}"> {{ $comic->title }} </a>
+            </div>
+        @endforeach
+    </div>
 </body>
 
 </html>
