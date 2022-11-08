02907c8aeced110f915012de4d6fd1e73ff0fbb7
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-08 21:17:26 +0100

    edit css

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="css/app.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
     <title>Singolo Fumetto {{ $comic->title }}</title>
 </head>
 
@@ -14,7 +17,7 @@
         <h1>{{ $comic->title }}</h1>
 
         <div>
-            <img src="{{ $comic->thumb }}" alt="{{ $comic->title }} cover">
+            <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
         </div>
 
         <p>{{ $comic->description }}</p>
