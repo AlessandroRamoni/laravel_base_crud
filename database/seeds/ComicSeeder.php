9b640b3295a1006e8128d77ae146e57de9c8af4f
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-08 15:53:14 +0100

    create framework

---
+++ database/seeds/ComicSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ComicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
