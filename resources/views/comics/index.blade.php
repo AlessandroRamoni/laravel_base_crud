aadfa32cdd5c0d237c299ee1389f5946d43fea2a
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-08 18:11:31 +0100

    add css

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,18 +5,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Luckiest+Guy&display=swap" rel="stylesheet">
     <title>Homepage Comics</title>
 </head>
 
 <body>
 
+    <h1>Benvenuto nel nostro store</h1>
     {{-- @dump($comics); --}}
     <div class="contenitore_homepage">
-        @foreach ($comics as $comic)
-            <div>
-                <a href="{{ route('comics.show', $comic->id) }}"> {{ $comic->title }} </a>
-            </div>
-        @endforeach
+        <div class="links">
+            <ul>
+                @foreach ($comics as $comic)
+                    <li><a href="{{ route('comics.show', $comic->id) }}"> {{ $comic->title }} </a> <span>
+                            <- clicca qui</span>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 </body>
 
