1081799ce3c090675169ab3a3b0a575fe63b4898
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-09 17:39:00 +0100

    edit table

--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -16,36 +16,36 @@
         @csrf
         @method('PUT')
         <div>
-            <label for="title">Title:</label>
+            <label for="title">Title</label>
             <input type="text" name="title" value="{{ old('title', $comic->title) }}">
         </div>
         <div>
-            <label for="description">Description:</label>
+            <label for="description">Description</label>
             <textarea name="description" cols="30" rows="10">{{ old('description', $comic->description) }}</textarea>
         </div>
         <div>
-            <label for="thumb">Thumbnail Link:</label>
+            <label for="thumb">Thumb</label>
             <input type="text" name="thumb" value="{{ old('thumb', $comic->thumb) }}">
         </div>
         <div>
-            <label for="price">Price: (use point . not comma , )</label>
+            <label for="price">Price</label>
             <input type="text" name="price" value="{{ old('price', $comic->price) }}">
         </div>
         <div>
-            <label for="series">Series:</label>
+            <label for="series">Series</label>
             <input type="text" name="series" value="{{ old('series', $comic->series) }}">
         </div>
         <div>
-            <label for="sale_date">Sale Date: (YYYY-MM-DD)</label>
+            <label for="sale_date">Sale Date</label>
             <input type="text" name="sale_date" value="{{ old('sale_date', $comic->sale_date) }}">
         </div>
         <div>
-            <label for="type">Type:</label>
+            <label for="type">Type</label>
             <input type="text" name="type" value="{{ old('type', $comic->type) }}">
         </div>
 
         <div>
-            <input type="submit" value="Submit Edited Infos">
+            <input type="submit" value="Aggiorna">
         </div>
 
     </form>
