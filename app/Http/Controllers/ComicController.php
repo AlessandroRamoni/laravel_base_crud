5c594686b599c6270684f56845739fcc0da627ee
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-09 17:38:40 +0100

    create update

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -81,6 +81,11 @@ class ComicController extends Controller
     public function update(Request $request, Comic $comic)
     {
         //
+
+        $data = $requets->all();
+        $comic->update($data);
+
+        return redirect()->route('comics.show', $comic->id);
     }
 
     /**
