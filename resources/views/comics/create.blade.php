30ca8e544fd2554a3bfd8f87fb74337911aeb2aa
AlessandroRamoni <alessandroramoni@yahoo.it>
2022-11-11 13:39:42 +0100

    add @if

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -10,48 +10,63 @@
 </head>
 
 <body>
+
+    @if ($errors->any())
+        <div>
+            Ci sono degli errori
+        </div>
+    @endif
+
     <h1>Aggiungi il fumetto che preferisci al nostro store!</h1>
     <div class="contenitore_form">
         <form method="POST" action="{{ route('comics.store') }}">
             @csrf
 
-            <div>
+            <div @error('title') class="is-invalid" @enderror>
                 <label for="title">Title</label>
-                <input type="text" name="title">
+                <input type="text" name="title" required maxlength="255" minlength="5"
+                    value="{{ old('title', '') }}">
+                @error('title')
+                    <div>
+                        {{ __('Sistema il titolo') }}
+                    </div>
+                @enderror
             </div>
 
             <div>
                 <label for="description">Description</label>
-                <textarea name="description" id="description" cols="30" rows="10"></textarea>
+                <textarea required name="description" id="description" cols="30" rows="10"
+                    value="{{ old('description', '') }}"></textarea>
             </div>
 
             <div>
                 <label for="thumb">Thumb</label>
-                <input type="url" name="thumb">
+                <input type="url" maxlength="255" required name="thumb" value="{{ old('thumb', '') }}">
             </div>
 
             <div>
                 <label for="price">Price</label>
-                <input type="number" step=".01" name="price">
+                <input type="number" max="999.99" required step=".01" name="price"
+                    value="{{ old('price', '') }}">
             </div>
 
             <div>
                 <label for="series">Series</label>
-                <input type="text" name="series">
+                <input type="text" maxlength="50" required name="series" value="{{ old('series', '') }}">
             </div>
 
             <div>
                 <label for="sale_date">Sale_date</label>
-                <input type="text" name="sale_date">
+                <input type="text" required name="sale_date" value="{{ old('sale_date', '') }}">
             </div>
 
             <div>
                 <label for="type">Type</label>
-                <input type="text" name="type">
+                <input type="text" maxlength="15" required name="type" value="{{ old('type', '') }}">
             </div>
 
             <div id="crea">
-                <input type="submit" value="Crea!">
+                <input type="submit" required value="Crea!">
             </div>
 
 
